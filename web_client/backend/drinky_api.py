cdb80359ba1d86cd1d894b8524a3f3fc3b16aa73
Josh Medlock <jmedsy@gmail.com>
2025-05-14 12:17:31 -0500

    first commit

---
+++ web_client/backend/drinky_api.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, origins=['http://localhost:3000'])
+@app.route('/api/send-text', methods=['POST'])
+
+def receive_text():
+    data = request.get_json()
+    text = data.get('text', '')
+
+    print(f"Received text from frontend:\n{text}")
+
+    # type_text_to_matrix(text)
+
+    return jsonify({'status': 'ok'})
+
+if __name__ == '__main__':
+    app.run(port=8000)
